97ac0eb1c18137400b6e6af7e06a92c475e8268d
Stas Borychevskyi <sborichevskiy74@gmail.com>
2025-07-03 14:07:00 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
 'use strict';
+import readline from 'node:readline';
+import { generateRandomNumber } from './modules/generateRandomNumber';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput';
+import { getBullsAndCows } from './modules/getBullsAndCows';
 
-// Write your code here
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const mainNumber = generateRandomNumber();
+
+function askGuess() {
+  rl.question('The number is ready, can you guess? ', (input) => {
+    if (checkIsValidUserInput(input)) {
+      const result = getBullsAndCows(+input, mainNumber);
+
+      if (result.bulls === 4) {
+        // eslint-disable-next-line no-console
+        console.log('Congrats! You have won!');
+        rl.close();
+      } else {
+        askGuess();
+      }
+    } else {
+      askGuess();
+    }
+  });
+}
+
+askGuess();
